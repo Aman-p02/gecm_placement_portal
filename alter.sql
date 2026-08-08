47b2ea68bd6d0fcfb43b684a637d2f4b27105d21
VanitDantani <dantanivanit8@gmail.com>
2026-08-08 11:37:22 +0530

    remove backup files

--- alter.sql
+++
@@ -1,2 +0,0 @@
-USE gec_placement_portal;
-ALTER TABLE tbl_student_profile ADD COLUMN active_backlogs INT DEFAULT 0 AFTER sem6_cpi;
--- alter4.sql
+++
@@ -1,4 +0,0 @@
-USE gec_placement_portal;
-
-ALTER TABLE tbl_students
-ADD COLUMN is_blocked BOOLEAN DEFAULT FALSE AFTER password;
